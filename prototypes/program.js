2b7f1d96de4013fe4670158e96431710f461453c
Сергей Александрович Ермоченко <YermochenkoSA@office.vsu.by>
2020-04-10 22:16:16 +0300

    JavaScript
    Обработка формы через реакцию на событие onsubmit

--- prototypes/program.js
+++ prototypes/program.js
@@ -1,23 +1,34 @@
-var buttonOKHandler = function() {
-	console.log('FUNCTION START');
-	console.log(this);
-	var a = parseInt(document.getElementById('number-a').value);
-	var b = parseInt(document.getElementById('number-b').value);
+var formSummHandler = function() {
+	//var a = parseFloat(document.getElementById('number-a').value);
+	//var b = parseFloat(document.getElementById('number-b').value);
+	var a = parseFloat(this['numberA'].value);
+	var b = parseFloat(this['numberB'].value);
+	var message = document.getElementById('block-message');
+	message.style.display = 'block';
 	if(!isNaN(a) && !isNaN(b)) { // NaN - Not a Number - спец. значение
-		console.log(a + b);
+		message.className = 'popup';
+		var s = '<div>' + a + ' + ' + b + ' = <span style="font-weight:bold; color:blue;">' + (a+b) + '</span></div>';
+		message.innerHTML += s;
+//		var div = document.createElement('div');
+//		var text = document.createTextNode(a + ' + ' + b + ' = ');
+//		div.appendChild(text);
+//		var span = document.createElement('span');
+//		span.style.fontWeight = 'bold';
+//		span.style.color = 'blue';
+//		var text = document.createTextNode(a + b);
+//		span.appendChild(text);
+//		div.appendChild(span);
+//		message.appendChild(div);
 	} else {
-		console.log('ERROR');
+		message.className = 'popup_danger';
+		message.innerText = 'ERROR';
 	}
+	return false;
 };
 
-var buttonOK = document.getElementById('button-ok');
-if(buttonOK) {
-	buttonOK.onclick = buttonOKHandler;
+var formSumm = document.getElementById('form-summ');
+if(formSumm) {
+	formSumm.onsubmit = formSummHandler;
+} else {
+	console.log('HANDLER ADDING ERROR');
 }
-
-var buttonTest = document.getElementById('button-test');
-if(buttonTest) {
-	buttonTest.onclick = buttonOKHandler;
-}
-
-buttonOKHandler();
