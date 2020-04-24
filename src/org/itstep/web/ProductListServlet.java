98863573cfacbc9b0e62b7bc9aa48f03a65fbed3
Сергей Александрович Ермоченко <YermochenkoSA@office.vsu.by>
2020-04-24 21:20:43 +0300

    сделано разделение на контроллер и представление

--- src/org/itstep/web/ProductListServlet.java
+++ src/org/itstep/web/ProductListServlet.java
@@ -1,7 +1,6 @@
 package org.itstep.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -29,36 +28,8 @@ public class ProductListServlet extends HttpServlet {
 		try(Factory factory = new Factory()) {
 			ProductService service = factory.getProductService();
 			List<Product> products = service.findAll();
-			resp.setCharacterEncoding("UTF-8");
-			PrintWriter pw = resp.getWriter();
-			pw.println("<!DOCTYPE html>");
-			pw.println("<html>");
-			pw.println("<head>");
-			pw.println("<meta charset=\"UTF-8\">");
-			pw.println("<title>Products list</title>");
-			pw.println("</head>");
-			pw.println("<body>");
-			pw.println("<h1>Products list</h1>");
-			pw.println("<table border=\"1\">");
-			pw.println("<tr>");
-			pw.println("<th>Категория</th>");
-			pw.println("<th>Название</th>");
-			pw.println("<th>Цена</th>");
-			pw.println("<th>Количество</th>");
-			pw.println("<th>Дата добавления</th>");
-			pw.println("</tr>");
-			for(Product product : products) {
-				pw.println("<tr>");
-				pw.printf("<td>%s</td>", product.getCategory().getName());
-				pw.printf("<td>%s</td>", product.getName());
-				pw.printf("<td>$%d.%02d</td>", product.getPrice() / 100, product.getPrice() % 100);
-				pw.printf("<td>%d шт.</td>", product.getAmount());
-				pw.printf("<td>%1$td.%1$tm.%1$tY, %1$tA</td>", product.getDate());
-				pw.println("</tr>");
-			}
-			pw.println("</table>");
-			pw.println("</body>");
-			pw.println("</html>");
+			req.setAttribute("products", products);
+			req.getRequestDispatcher("/WEB-INF/product/list.html").forward(req, resp);
 		} catch(LogicException e) {
 			throw new ServletException(e);
 		}
