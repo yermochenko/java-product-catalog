a8eb64a7672668ea08dee809722a569369552a07
Сергей Александрович Ермоченко <YermochenkoSA@office.vsu.by>
2020-07-28 11:44:33 +0300

    Рефакторинг

--- src/org/itstep/domain/Entity.java
+++ src/org/itstep/domain/Entity.java
@@ -12,4 +12,28 @@ abstract public class Entity implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	@Override
+	public int hashCode() {
+		return (id == null) ? 0 : id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this != obj) {
+			if(obj != null && getClass() != obj.getClass()) {
+				Entity other = (Entity)obj;
+				if(id == null) {
+					if(other.id != null) {
+						return false;
+					}
+				} else if (!id.equals(other.id)) {
+					return false;
+				}
+				return true;
+			}
+			return false;
+		}
+		return true;
+	}
 }
